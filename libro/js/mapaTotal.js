a6f1609a4825943f5f96ee6948cb716ce5636915
MishellSalinas <mishelle-95@hotmail.com>
2018-12-26 14:32:53 -0500

    Libro completo

--- libro/js/mapaTotal.js
+++ libro/js/mapaTotal.js
@@ -1,23 +1,32 @@
 require([
   "esri/Map",
   "esri/views/MapView",
-  "esri/layers/FeatureLayer"
+  "esri/layers/FeatureLayer",
+  "esri/layers/VectorTileLayer"
 ], function(
   Map,
   MapView,
-  FeatureLayer
+  FeatureLayer,
+  VectorTileLayer
 ) {
 
   const hurricanesLayer = new FeatureLayer({
     url: "https://services.arcgis.com/8DAUcrpQcpyLMznu/ArcGIS/rest/services/Lugares_Nacimiento_Romanticismo/FeatureServer/0",
     outFields: ["*"]
   });
-
-  const map = new Map({
-    basemap: "dark-gray",
-    layers: [hurricanesLayer]
+ const paises = new FeatureLayer({
+    url: "https://services.arcgis.com/8DAUcrpQcpyLMznu/ArcGIS/rest/services/Mapa_1851/FeatureServer/0",
+    outFields: ["*"]
   });
 
+  const map = new Map({
+  });
+  var tileLayer = new VectorTileLayer({
+        url: "https://jsapi.maps.arcgis.com/sharing/rest/content/items/251955cdd7f7495cbeac50bf37b6c4e1/resources/styles/root.json"
+      });
+      map.add(tileLayer);
+      map.add(paises);
+      map.add(hurricanesLayer);
   const view = new MapView({
     container: "mapaTotal",
     map: map,
@@ -27,7 +36,118 @@ require([
       color: "orange"
     }
   });
-
+  document.getElementById("Alemania").addEventListener("click",
+        function() {
+          view.goTo({
+      center: [9.8, 50.2],
+      zoom: 4
+    });
+        });
+  document.getElementById("Francia").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [4, 47],
+      zoom: 4.1
+      });
+  });
+  document.getElementById("Espana").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-2, 40],
+      zoom: 4
+      });
+  });
+  document.getElementById("Inglaterra").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-4, 54],
+      zoom: 4
+      });
+  });
+  document.getElementById("Portugal").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-12, 40],
+      zoom: 2
+      });
+  });
+  document.getElementById("Rusia").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [40, 60],
+      zoom: 2.7
+      });
+  });
+  document.getElementById("Usa").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-100, 45],
+      zoom: 2
+      });
+  });
+  document.getElementById("Mexico").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-100, 25],
+      zoom: 3
+      });
+  });
+  document.getElementById("Colombia").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-74, 4],
+      zoom: 4.3
+      });
+  });
+  document.getElementById("Argentina").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [-60, -44],
+      zoom: 3
+      });
+  });
+  document.getElementById("Italia").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [12, 42],
+      zoom: 4
+      });
+  });
+  document.getElementById("Escandinavia").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [14, 60],
+      zoom: 2.5
+      });
+  });
+  document.getElementById("Paises bajos").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [5.5, 51.2],
+      zoom: 5
+      });
+  });
+  document.getElementById("Polonia").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [20, 51.2],
+      zoom: 4
+      });
+  });
+  document.getElementById("Checa").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [15.7, 50],
+      zoom: 4
+      });
+  });
+  document.getElementById("Hungria").addEventListener("click",
+      function() {
+      view.goTo({
+      center: [22, 46],
+      zoom: 4
+      });
+  });
   view.ui.add("info");
 
   view.when().then(function() {
@@ -74,7 +194,9 @@ require([
         const ano = attributes.ano_nac;
         const id = attributes.OBJECTID;
         const imagen = attributes.url_imagen;
-
+        const anoMuerte = attributes.ano_muerte;
+        const fechaMuerte = attributes.fecha_m;
+        const referencia = attributes.referencia;
         if (highlight && (currentName !== name || currentYear !==
             year)) {
           highlight.remove();
@@ -89,7 +211,9 @@ require([
         document.getElementById("fechaNto").innerHTML = fecha;
         document.getElementById("ano_nac").innerHTML = ano;
         document.getElementById("imagen").src = imagen;
-
+        document.getElementById("referencia").innerHTML = "<a href='"+referencia+"'>Ver más</a>"
+        document.getElementById("anoMuerte").innerHTML = anoMuerte;
+        document.getElementById("fechaMuerte").innerHTML = "-" +fechaMuerte;
         // highlight all features belonging to the same hurricane as the feature
         // returned from the hitTest
         const query = layerView.layer.createQuery();
